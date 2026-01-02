09d0eda6886242220ee6dfc3741483e0c142c7a0
sf27sf27 <sfullerton027@gmail.com>
2026-01-02 12:22:06 -0500

    Remove submodule reference and consolidate api directory

---
+++ api/sensor_project/sensors/bme280.py
@@ -0,0 +1,38 @@
+import time
+import board
+import busio
+import adafruit_bme280.basic as adafruit_bme280
+
+# Initialize I2C
+i2c = busio.I2C(board.SCL, board.SDA)
+
+# If address is 0x76 (most common)
+bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x77)
+
+# Optional: set sea level pressure for altitude accuracy
+bme280.sea_level_pressure = 1013.25
+
+
+def read():
+	try:
+		t = bme280.temperature
+		p = bme280.pressure
+		h = bme280.humidity
+		temp_data = {
+			"c":round(t,2),
+			"f":round(t*1.8+32,2)
+		}
+		pressure_data = {
+			"hpa":round(p,2)
+		}
+		humidity_data = {
+			"rh":round(h,2)
+		}
+		data = {
+			"temperature":temp_data,
+			"pressure":pressure_data,
+			"humidity":humidity_data
+		}
+		return data
+	except Exception:
+                return "Error"
---
+++ api/sensor_project/sensors/disk_space.py
@@ -0,0 +1,18 @@
+
+import shutil
+
+def bytes_to_mb(b):
+	return round(b / (1024**2), 2)
+def read():
+	output = shutil.disk_usage("/")
+	total = output.total
+	used = output.used
+	free = output.free
+	data = {
+		"total_mb":bytes_to_mb(total),
+		"used_mb":bytes_to_mb(used),
+		"free_mb":bytes_to_mb(free)
+	}
+
+	return data
+
