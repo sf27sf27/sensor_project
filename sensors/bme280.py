1614b588f3a5a357f45b3db42e420f537d780aef
sf27sf27 <sfullerton027@gmail.com>
2026-01-20 21:45:25 -0500

    graceful bme280 error handling

--- sensors/bme280.py
+++ sensors/bme280.py
@@ -1,20 +1,35 @@
 import time
 
+HARDWARE_AVAILABLE = False
+bme280 = None
+
 try:
 	import board
 	import busio
 	import adafruit_bme280.basic as adafruit_bme280
-	HARDWARE_AVAILABLE = True
 	
 	# Initialize I2C
 	i2c = busio.I2C(board.SCL, board.SDA)
 	
-	# If address is 0x76 (most common)
-	bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=0x77)
+	# Try to find BME280 at either address (0x77 or 0x76)
+	bme280 = None
+	for address in [0x77, 0x76]:
+		try:
+			bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c, address=address)
+			break
+		except ValueError:
+			# This address doesn't have the device, try next
+			continue
+	
+	if bme280 is None:
+		raise ValueError("No BME280 device found at addresses 0x76 or 0x77")
 	
 	# Optional: set sea level pressure for altitude accuracy
 	bme280.sea_level_pressure = 1013.25
-except (NotImplementedError, ImportError):
+	HARDWARE_AVAILABLE = True
+except Exception as e:
+	# Catch any exception during hardware initialization
+	# This includes: ImportError, NotImplementedError, OSError, ValueError, etc.
 	HARDWARE_AVAILABLE = False
 	bme280 = None
 
